f5145a173ec32d533cd76eafe7bd67aecfe59359
Arfanur <arfanur03@gmail.com>
2022-06-29 14:47:13 +0600

    check

---
+++ 6_scope.js
@@ -0,0 +1,11 @@
+const favNum = 27;
+
+function add(first, second) {
+    const result = first + second;
+    console.log(result);
+    return result;
+}
+const sum = add(11, 35); 
+console.log(favNum);
+
+// check
