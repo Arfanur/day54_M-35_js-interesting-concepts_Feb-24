7b48a40fef9b0db9dacd5efb54136abee336500c
Arfanur <arfanur03@gmail.com>
2022-07-02 20:36:25 +0600

    M-35_6_scope

--- 6_scope.js
+++ 6_scope.js
@@ -1,11 +1,20 @@
 const favNum = 27;
 
 function add(first, second) {
+    // console.log(mood);
     const result = first + second;
-    console.log(result);
+    if (result > 9) {
+        let mood = 'happy';
+        mood = 'cranky';
+    }
+    // console.log(mood);
     return result;
 }
-const sum = add(11, 35); 
-console.log(favNum);
+const sum = add(11, 35);
 
-// check
+// console.log(mood);
+
+for (let i = 0; i < 10; i++) {
+
+}
+console.log(i);
